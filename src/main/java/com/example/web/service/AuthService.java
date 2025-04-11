fd2f969655025933ebbea2039d8ed0327fc97276
P-aan0901 <22130004@st.hcmuaf.edu.vn>
2025-04-11 21:23:09 +0700

    Update backend forgot password

--- src/main/java/com/example/web/service/AuthService.java
+++ src/main/java/com/example/web/service/AuthService.java
@@ -8,6 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 public class AuthService {
@@ -27,17 +28,17 @@ public class AuthService {
             return false;
         }
         User user = udao.findByEmail(email);
+        long time = 24 * 60 * 60 * 1000;
         if (user != null) {
             String token = UUID.randomUUID().toString();
-            udao.saveTokenForRegister(user.getId(), token, "register");
+            udao.saveTokenForRegister(user.getId(), token, "register", time);
 
             String subject = "Xác nhận email đăng ký";
             String body = "Xin chào " + fullName + ",\n\nCó phải bạn vừa đăng ký tài khoản? " +
                     "Nếu đúng, vui lòng nhấp vào liên kết dưới đây để xác nhận email của bạn và hoàn tất quá trình đăng ký:\n\n"
                     + "http://localhost:8080/TTLTW_war/activate_account?token=" + token + "\n\n"
                     + "Nếu bạn không thực hiện đăng ký này, vui lòng bỏ qua email này.";
-            emailService.sendEmail(email, subject, body);
-            return true;
+            return  emailService.sendEmail(email, subject, body);
         }
         return false;
     }
@@ -58,6 +59,29 @@ public class AuthService {
                 + "http://localhost:8080/TTLTW_war/activate_account?token=" + token + "\n\n"
                 + "Nếu không phải bạn, vui lòng bỏ qua email này.";
         emailService.sendEmail(user.getEmail(), subject, body);
+    }
+    public boolean hasValidToken(User user, String type) {
+        return udao.hasValidToken(user, type);
+    }
+
+    public boolean passwordRecovery(String email) throws SQLException {
+        User user = udao.findByEmail(email);
+        if (user == null) {
+            return false;
+        }
+        long time = 15 * 60 * 1000;
+        String token = UUID.randomUUID().toString();
+        udao.saveTokenForRegister(user.getId(), token, "forgotPass", time);
+
+        String resetLink = "http://localhost:8080/TTLTW_war/reset_password?token=" + token;
+
+        String subject = "Yêu cầu đặt lại mật khẩu";
+        String body = "Chào bạn,\n\n"
+                + "Bạn đã yêu cầu đặt lại mật khẩu. Nhấn vào link bên dưới để đặt lại mật khẩu:\n"
+                + resetLink + "\n\n"
+                + "Nếu bạn không yêu cầu hành động này, vui lòng bỏ qua email.";
+
+        return emailService.sendEmail(user.getEmail(), subject, body);
     }
     public boolean createUserByGoogle(String gg_id, String name, String email) throws SQLException {
         return udao.createUserByGoogle(gg_id, name, email, 2);
@@ -109,17 +133,33 @@ public class AuthService {
     public boolean updateUserInfo(User user) throws SQLException {
         return udao.updateUserInfo(user);
     }
+    public String handleResetPassword(String token, String newPassword) throws SQLException {
+        UserToken userToken = findByToken(token, "forgotPass");
+
+        if (userToken == null || userToken.getExpiredAt().before(new Timestamp(System.currentTimeMillis()))) {
+            return "token_expired";
+        }
+        User user = udao.getUser(userToken.getUserId());
+        if (user == null) {
+            return "token_expired";
+        }
+        String hashedNewPassword = hashPassword(newPassword);
+
+        if (hashedNewPassword.equals(user.getPassword())) {
+            return "same_password";
+        }
+        boolean updated = udao.updatePassword(user.getId(), hashedNewPassword);
+
+        if (updated) {
+            udao.deleteRegisterToken(token, "forgotPass");
+            return "success";
+        } else {
+            return "update_failed";
+        }
+    }
 
     public static void main(String[] args) throws SQLException {
         AuthService a = new AuthService();
         System.out.println(a.checkLogin("admin", "462004"));
-    }
-
-    public boolean hasValidToken(User user, String type) {
-        return udao.hasValidToken(user, type);
-    }
-
-    public boolean passwordRecovery(String email) throws SQLException {
-        return udao.passwordRecovery(email);
     }
 }
