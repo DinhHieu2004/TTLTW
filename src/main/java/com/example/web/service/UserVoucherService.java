a5a3a171a742fbc13a89a56ebdbc1a9fa5cd63f3
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-04-16 20:29:39 +0700

    model user voucher giảm giá của riêng user

---
+++ src/main/java/com/example/web/service/UserVoucherService.java
@@ -0,0 +1,27 @@
+package com.example.web.service;
+
+import com.example.web.dao.UserVoucherDao;
+import com.example.web.dao.model.UserVoucher;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserVoucherService {
+    UserVoucherDao userVoucherDao = new UserVoucherDao();
+
+    public List<UserVoucher> getAllByUserId(int userId) throws SQLException {
+        return userVoucherDao.getUserVoucherById(userId);
+    }
+
+    public List<UserVoucher> getUnusedVouchersByUser(int userId) throws SQLException {
+        return userVoucherDao.getUnusedVouchersByUser(userId);
+    }
+
+    public boolean assignVoucher(int userId, int voucherId) throws SQLException {
+        return userVoucherDao.assignVoucherToUser(userId, voucherId);
+    }
+
+    public boolean markAsUsed(int userVoucherId) throws SQLException {
+        return userVoucherDao.markVoucherAsUsed(userVoucherId);
+    }
+}
+
